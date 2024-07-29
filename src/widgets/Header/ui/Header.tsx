a3ea8dae208a5ac97202f0f3b648ce3db721c512
DanilGrebnev <grebnevdanil60@gmail.com>
2024-07-29 13:41:50 +0300

    feat: global search refactoring

--- src/widgets/Header/ui/Header.tsx
+++ src/widgets/Header/ui/Header.tsx
@@ -2,13 +2,13 @@ import { SearchInput } from '@/entities/posts'
 import { UserProfileIcon } from '@/features/user'
 
 import { NavigationGroup } from './NavigationGroup'
-import s from './s.module.sass'
+import s from './s.module.scss'
 
 export const Header = () => {
     return (
         <header className={s.header}>
             <NavigationGroup className={s['nav-group']} />
-            <SearchInput className={s['search']} />
+            <SearchInput className={s.search} />
             <div className={s['user-bar']}>
                 <UserProfileIcon />
             </div>
